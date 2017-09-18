aecdb617077ed4a33afe33433c3f09fdd0f6b6b2
horaciocoronel <arielc@gmail.com>
2017-09-18 14:20:35 -0400

    First part of exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,11 @@
+# Using gets
+
+puts "What is your name?"
+user_name = gets
+puts "Hello, #{user_name}"
+
+# Using gets.chomp
+
+puts "What is your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}"
