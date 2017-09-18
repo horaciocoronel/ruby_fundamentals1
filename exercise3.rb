aab5505775ba539cb864185cc0e038e69acdfee5
horaciocoronel <arielc@gmail.com>
2017-09-18 14:38:20 -0400

    Get year from user and return DOB - Finish exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,11 +1,22 @@
-# Using gets
+require 'date'
 
-puts "What is your name?"
-user_name = gets
-puts "Hello, #{user_name}"
+# # Using gets
 
-# Using gets.chomp
+# puts "What is your name?"
+# user_name = gets
+# puts "Hello, #{user_name}"
+
+# # Using gets.chomp
 
 puts "What is your name?"
 user_name = gets.chomp
 puts "Hello, #{user_name}"
+
+# Now try asking your user how old they are and 
+# have your program output what year they were born in.
+
+puts "How old are you?"
+user_age = gets.chomp
+user_age = user_age.to_i
+year = Date.today.year
+puts "You were born in #{year - user_age}"
