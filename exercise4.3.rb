e04fde8919cc74deb91c243a45ffe6c3e5164404
horaciocoronel <arielc@gmail.com>
2017-09-18 15:10:38 -0400

    Finish exercise 4.3

---
+++ exercise4.3.rb
@@ -0,0 +1,8 @@
+my_name = "Horacio"
+puts "What'\s your name?"
+user_name = gets.chomp
+if my_name == user_name
+	puts "We have the same name!"
+else
+	puts "We don'\t have the same name, but yours ir also cool."
+end
