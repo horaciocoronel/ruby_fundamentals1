6cc2e34ed83d3ba350aa0d29c0ebeebb0c50d638
horaciocoronel <arielc@gmail.com>
2017-09-18 14:03:34 -0400

    Finish point 2 of exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -7,3 +7,17 @@ puts "A good tip would be $#{meal * 20.0 / 100}"
 puts "An OK tip would be $#{meal * 15.0 / 100}"
 puts "A 'Service could be better'-tip would be $#{meal * 10.0 /100}"
 puts "A 'I won't come back'-tip would be $#{meal * 0.0 / 100}"
+
+# 2. Try adding a string and an integer with the + operator. 
+# 	 What happens? Find a way to convert the integer into a string 
+# 	 first and use puts to print the result.
+
+meal = 50 + "5".to_i
+
+puts meal
+puts "------ Exercise #2 ------"
+puts "A great tip would be $#{meal * "25.0".to_i / 100}"
+puts "A good tip would be $#{meal * "20.0".to_i / 100}"
+puts "An OK tip would be $#{meal * "15.0".to_i / 100}"
+puts "A 'Service could be better'-tip would be $#{meal * "10.0".to_i /100}"
+puts "A 'I won't come back'-tip would be $#{meal * "0.0".to_i / 100}"
