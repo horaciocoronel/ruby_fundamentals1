d6295ca1dbeeabf600a6649e71defed4e2ce241c
horaciocoronel <arielc@gmail.com>
2017-09-18 13:27:23 -0400

    Complete first excerice

---
+++ exercise1.rb
@@ -0,0 +1,38 @@
+2 + 3
+
+puts 2 + 3
+
+puts 2
+puts 3
+puts 2 + 3
+
+## ERROR LINE when: puts 2 puts 3 puts 2 + 3 ##
+
+#-----------------------------#
+
+true && true # true
+true && false # false
+true && !false # true
+false && false # false
+
+true || false # true
+true || true # true
+false || true # true
+false || false # false
+!false || false # true
+
+
+
+(1 < 3) && (3 < 5)
+# true
+
+(1 > 1) && (2 > 2)
+# false
+
+(1 == 2) || (2 < 3)
+# true
+
+(1 != 2) || (2 < 3)
+# true
+
+(1 == 2) || (2 == 3)
